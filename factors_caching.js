f86d0d1e29979db4011465610b968b6f97a1fc85
sunghyukpark <sunghyukpark0408@gmail.com>
2016-02-07 23:24:32 -0600

    refactor makeNumFactorSet

--- factors_caching.js
+++ factors_caching.js
@@ -1,35 +1,47 @@
-// var memoizer = function(callback){
-//   var cache = [];
-//   return function(n){
-//     var idx = n.toString();
-//     if(cache[idx] == undefined){
-//       cache[idx] = callback(n);
-//     }
+var memoizer = function(callback){
+  var cache = [];
+  return function(n){
+    var idx = n.toString();
+    if(cache[idx] == undefined){
+      cache[idx] = callback(n);
+    }
 
-//     return cache[idx];
-//   };
-// }
+    return cache[idx];
+  };
+}
 
 
-var findFactors = function(numArr){
+var checkArrayLength = function(arr){
+  if (arr.length == 0)
+    throw new Error("Invalid input. Should have at least 1 element")
+}
+
+
+
+var findFactors = function(num, arr){
+  checkArrayLength(arr);
+
+  var len = arr.length;
+  var factors = [];
+
+  for(var j=0; j< len; j++){
+    if (num % arr[j] == 0 && num != arr[j]){
+      factors.push(arr[j])
+    }
+  }
+  return factors;
+}
+
+
+var makeNumFactorSet = function(numArr){
+  checkArrayLength(numArr);
+
   var arrLen = numArr.length;
-
-  if(arrLen == 0)
-    throw new Error("Invalid input, Should have at least 1 element")
-
   var factorSet = {}
 
   for(var i=0; i< arrLen; i++){
-    var factors = []
-
-    for(var j=0; j< arrLen; j++){
-      if (numArr[i] % numArr[j] == 0 && numArr[i] != numArr[j]){
-        factors.push(numArr[j])
-      }
-    }
-    factorSet[numArr[i]] = factors;
+    factorSet[numArr[i]] = findFactors(numArr[i], numArr)
   }
-
   return factorSet;
 }
 
@@ -46,8 +58,8 @@ var sortNumber = function(a,b){
 // Test cases
 var arr = [20,5,2,7,10];
 var arr2 = [2,5,10]
-var testSet = findFactors(arr);
-var testSet2 = findFactors(arr2);
+var testSet = makeNumFactorSet(arr);
+var testSet2 = makeNumFactorSet(arr2);
 displayFactors(testSet);
 displayFactors(testSet2);
 
