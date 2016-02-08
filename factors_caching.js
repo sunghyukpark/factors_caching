dcf740d792cf724c2bed440a97eb8429a7f1564d
sunghyukpark <sunghyukpark0408@gmail.com>
2016-02-08 02:36:17 -0600

    with caching, without caching both successful

--- factors_caching.js
+++ factors_caching.js
@@ -1,21 +1,7 @@
-var memoizer = function(callback){
-  var cache = [];
-  return function(n){
-    var idx = n.toString();
-    if(cache[idx] == undefined){
-      cache[idx] = callback(n);
-    }
-
-    return cache[idx];
-  };
-}
-
-
 var checkArrayLength = function(arr){
   if (arr.length == 0)
     throw new Error("Invalid input. Should have at least 1 element")
 }
-
 
 
 var findFactors = function(num, arr){
@@ -33,14 +19,63 @@ var findFactors = function(num, arr){
 }
 
 
-var makeNumFactorSet = function(numArr){
-  checkArrayLength(numArr);
+var cacheFactors = function(cache, num, arr){
+  if(cache[num] == undefined)
+    cache[num] = findFactors(num, arr);
+}
 
+
+var makeFactorSet = function(numArr){
+  checkArrayLength(numArr);
   var arrLen = numArr.length;
-  var factorSet = {}
+  var factorSet = {};
 
   for(var i=0; i< arrLen; i++){
-    factorSet[numArr[i]] = findFactors(numArr[i], numArr)
+    var mightBeFactors = numArr.slice(i,arrLen);
+    var num = numArr[i];
+    var factors = findFactors(num, mightBeFactors);
+
+    factorSet[num] = factors;
+  }
+  return factorSet;
+}
+
+var cacheMakeFactorSet = function(numArr){
+  checkArrayLength(numArr);
+
+  // sort in descending order to utilize caching
+  numArr.sort(sortDesc);
+
+  var cache = [];
+  var arrLen = numArr.length;
+  var factorSet = {};
+
+  for(var i=0; i< arrLen; i++){
+
+    // since array is sorted, no need to look for elements with smaller index
+    var mightBeFactors = numArr.slice(i,arrLen)
+    var num = numArr[i]
+
+    // cache[20] = [10,5,2] if it's undefined
+    cacheFactors(cache, num, mightBeFactors);
+
+    // after caching [10,5,2], cache factors of 10,5,2 into cache
+    // cache[10], cache[5], cache[2]
+    var clen = cache[num].length;
+    if (clen > 0){
+      for(var j=0; j< clen; j++){
+        var factor = cache[num][j];
+        var idx = numArr.indexOf(factor);
+        var fmightBeFactors = numArr.slice(idx, arrLen)
+        cacheFactors(cache, factor, fmightBeFactors);
+      }
+    }
+
+    // console.log(num)
+    // console.log(cache)
+    // console.log(" ")
+
+    factorSet[num] = cache[num];
   }
   return factorSet;
 }
@@ -50,16 +85,17 @@ var displayFactors = function(factorSet){
   console.log(factorSet);
 }
 
-var sortNumber = function(a,b){
-  return a - b;
+
+var sortDesc = function(a,b){
+  return b - a;
 }
 
 
 // Test cases
-var arr = [20,5,2,7,10];
-var arr2 = [2,5,10]
-var testSet = makeNumFactorSet(arr);
-var testSet2 = makeNumFactorSet(arr2);
+var arr = [20,10,7,5,2];
+var cacheTestSet = cacheMakeFactorSet(arr);
+var testSet = makeFactorSet(arr);
+
+displayFactors(cacheTestSet);
 displayFactors(testSet);
-displayFactors(testSet2);
 
